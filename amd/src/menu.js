60c45d8d72419c2a94778b2317ddb0e8ae641f39
Quentin Fombaron <q.fombaron@hotmail.fr>
2018-06-28 09:19:31 +0200

    Add table of reactions

--- amd/src/menu.js
+++ amd/src/menu.js
@@ -1,27 +1,30 @@
 /* Include JQuery */
 define(['jquery'], function($) {
     return {
-        init: function() {
-            var openRow;
+        init: function(moduleIds) {
+            var openRow = [];
             /**
              *
              * @param event
              */
             function rowDetails(event) {
-                if (openRow === event.data.id) {
-                    $('.row_' + event.data.id + '_details').css({'display': 'none'});
-                    openRow = null;
+                if (openRow[event.data.id]) {
+                    $('.row_module' + event.data.id + '_details').css({'display': 'none'});
+                    $('.row_module' + event.data.id + ' .c5').html('+');
+                    openRow[event.data.id] = false;
                 } else {
-                    $('.row_' + event.data.id + '_details').css({'display': ''});
-                    openRow = event.data.id;
+                    $('.row_module' + event.data.id + '_details').css({'display': ''});
+                    $('.row_module' + event.data.id + ' .c5').html('-');
+                    openRow[event.data.id] = true;
                 }
             }
 
             $(function() {
-                for (var i = 1; i <= 4; i++) {
-                    $('.row_' + i + '_details').css({'display': 'none'});
-                    $('.row_' + i + ' .c3').click({id: i}, rowDetails);
-                }
+                moduleIds.forEach(function(id) {
+                    openRow[id] = false;
+                    $('.row_module' + id + '_details').css({'display': 'none'});
+                    $('.row_module' + id + ' .c5').click({id: id}, rowDetails);
+                });
             });
         }
     };
--- menu.php
+++ menu.php
@@ -26,6 +26,8 @@
 require_once(__DIR__ . '/../../config.php');
 require_once($CFG->dirroot . '/blocks/like/lib.php');
 
+
+
 try {
     $id = required_param('instanceid', PARAM_INT);
     $courseid = required_param('courseid', PARAM_INT);
@@ -34,6 +36,7 @@ try {
     $group = optional_param('group', 0, PARAM_INT);
     $tab = optional_param('tab', 'overview', PARAM_ALPHA);
 
+
     $course = $DB->get_record('course', array('id' => $courseid), '*', MUST_EXIST);
 
     $context = CONTEXT_COURSE::instance($courseid);
@@ -41,6 +44,18 @@ try {
     $block = $DB->get_record('block_instances', array('id' => $id), '*', MUST_EXIST);
 
     $blockcontext = CONTEXT_BLOCK::instance($id);
+
+    try {
+        require_login();
+        require_capability('block/like:overview', $blockcontext);
+        confirm_sesskey();
+    } catch (coding_exception $e) {
+        echo 'Exception coding_exception (require_login() -> blocks/like/menu.php) : ', $e->getMessage(), "\n";
+    } catch (require_login_exception $e) {
+        echo 'Exception require_login_exception (require_login() -> blocks/like/menu.php) : ', $e->getMessage(), "\n";
+    } catch (moodle_exception $e) {
+        echo 'Exception moodle_exception (require_login() -> blocks/like/menu.php) : ', $e->getMessage(), "\n";
+    }
 
     $PAGE->set_course($course);
     $PAGE->set_url(
@@ -55,9 +70,11 @@ try {
         )
     );
 
+    $activities = block_like_get_activities($courseid, $config);
+
     $PAGE->set_context($context);
     $PAGE->requires->css(new moodle_url($CFG->wwwroot . '/blocks/like/style/style.css'));
-    $paramsamd = array();
+    $paramsamd = array(array_column($activities, 'id'));
     $PAGE->requires->js_call_amd('block_like/menu', 'init', $paramsamd);
     $title = get_string('menu', 'block_like');
     $PAGE->set_title($title);
@@ -65,38 +82,68 @@ try {
     $PAGE->navbar->add($title);
     $PAGE->set_pagelayout('report');
 
-    try {
-        require_login($course, false);
-        require_capability('block/like:overview', $blockcontext);
-        confirm_sesskey();
-    } catch (coding_exception $e) {
-        echo 'Exception coding_exception (require_login() -> blocks/like/menu.php) : ', $e->getMessage(), "\n";
-    } catch (require_login_exception $e) {
-        echo 'Exception require_login_exception (require_login() -> blocks/like/menu.php) : ', $e->getMessage(), "\n";
-    } catch (moodle_exception $e) {
-        echo 'Exception moodle_exception (require_login() -> blocks/like/menu.php) : ', $e->getMessage(), "\n";
-    }
-
     echo $OUTPUT->header();
     echo $OUTPUT->heading($title, 2);
     echo $OUTPUT->container_start('block_like');
 
     require("tabs.php");
 
-    $table = new html_table();
-    $table->head = array('Modules', 'Maximum grade', 'Grade to pass', '');
-    $table->attributes['class'] = 'generaltable';
-    $table->data = array();
-    $table->rowclasses = array(
-        'row_1', 'row_1_details',
-        'row_2', 'row_2_details',
-        'row_3', 'row_3_details',
-        'row_4', 'row_4_details'
-    );
-    $md = range(1, 8);
+    $sql = 'SELECT Base.cmid, Base.type, 
+            IFNULL(COUNT(Base.cmid), 0) AS total,
+            IFNULL(TableTypeOne.TotalTypeOne, 0) AS typeone,
+            IFNULL(TableTypeTwo.TotalTypeTwo, 0) AS typetwo,
+            IFNULL(TableTypeThree.TotalTypethree, 0) AS typethree
+          FROM {block_like} AS Base
+            NATURAL LEFT JOIN (SELECT cmid, COUNT(vote) AS TotalTypeOne FROM {block_like}
+              WHERE vote = 1 GROUP BY cmid) AS TableTypeOne
+            NATURAL LEFT JOIN (SELECT cmid, COUNT(vote) AS TotalTypeTwo FROM {block_like}
+              WHERE vote = 2 GROUP BY cmid) AS TableTypeTwo
+            NATURAL LEFT JOIN (SELECT cmid, COUNT(vote) AS TotalTypethree FROM {block_like}
+              WHERE vote = 3 GROUP BY cmid) AS TableTypeThree
+          GROUP BY cmid;';
 
-    foreach ($md as $mds) {
-        array_push($table->data, array('1', '2', '3', 'V'));
+    $params = array('userid' => $USER->id);
+
+    try {
+        $result = $DB->get_records_sql($sql, $params);
+    } catch (dml_exception $e) {
+        echo 'Exception : ', $e->getMessage(), "\n";
+    }
+
+    $table = new html_table();
+    $table->head = array('Cours', 'Modules', '', 'Réactions', '', '');
+    $table->attributes['class'] = 'generaltable';
+    $table->rowclasses = array();
+    $table->data = array();
+
+    foreach ($activities as $index => $activity) {
+        if ($activity['type'] != 'label' && !is_null($result[($activity['id'])]->cmid)) {
+            array_push($table->rowclasses,
+                'row_module' . $activity['id'],
+                'row_module' . $activity['id'] . '_details'
+            );
+            $attributes = ['class' => 'iconlarge activityicon'];
+            $icon = $OUTPUT->pix_icon('icon', $activity['modulename'], $activity['type'], $attributes);
+            array_push($table->data,
+                array(
+                    get_section_name($COURSE, $activity['section']),
+                    $icon . format_string($activity['name']),
+                    html_writer::empty_tag(
+                        'img',
+                        array('src' => $CFG->wwwroot . '/blocks/like/pix/easy.png', 'class' => 'overview_img')).' '.
+                    100 * intval($result[($activity['id'])]->typeone) / intval($result[($activity['id'])]->total).'% ',
+                    html_writer::empty_tag(
+                        'img',
+                        array('src' => $CFG->wwwroot . '/blocks/like/pix/better.png', 'class' => 'overview_img')).' '.
+                    100 * intval($result[($activity['id'])]->typetwo) / intval($result[($activity['id'])]->total).'% ',
+                    html_writer::empty_tag(
+                        'img',
+                        array('src' => $CFG->wwwroot . '/blocks/like/pix/hard.png', 'class' => 'overview_img')).' '.
+                    100 * intval($result[($activity['id'])]->typethree) / intval($result[($activity['id'])]->total).'% ',
+                    '+'
+                ),
+                array('', '', '', '', '', ''));
+        }
     }
 
     echo html_writer::table($table);
