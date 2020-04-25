d1a0ae3d747c51d813aa7acdfd2a39523a8a5edb
Quentin Fombaron <q.fombaron@hotmail.fr>
2020-04-25 18:27:29 +0200

    Fix version

--- version.php
+++ version.php
@@ -27,7 +27,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_point_view';
-$plugin->version   = 2020041701;
+$plugin->version   = 2020042501;
 $plugin->requires  = 2017051502;
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = 'v1.2';
+$plugin->release = 'v1.3';
